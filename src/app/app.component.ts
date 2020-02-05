c10e54ed4e47029ee94ab6c4a2b2906c97307f60
Amrita Choudhury <amrita.c333@gmail.com>
2020-02-05 16:57:48 -0600

    Now showing the activities selected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { concat, combineLatest } from 'rxjs';
 import { stringify } from 'querystring';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-root',
@@ -8,25 +9,35 @@ import { stringify } from 'querystring';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  public i = 1;
+  public newActivityListIndex = 1;
   public error = false;
+  public notSelected = false;
+  public done = false;
   public newActivity: string;
-
-  public newActivityList: Array<{id: number, value: string, checked: boolean}> = [];
-  
-  public done = false; // done = false means activity has not yet been added
-  // public show = true; // when show = true, the first Label will be visible
-                      // and the checkboxes will be invisible
+  public newActivityChk: boolean;
+  public strikeFlag: boolean;
+  public selectedActivityArr: Array<string> = [];
+  public newActivityList: Array<{
+    id: number;
+    value: string;
+    checked: boolean;
+  }> = [];
 
   public addMore() {
-
+    this.notSelected = false;
     this.done = false;
-
     if (this.newActivity && this.newActivity.trim()) {
-      if (!this.newActivityList.find((activity) => activity.value === this.newActivity)) {
-        this.newActivityList.push({id: this.i, value: this.newActivity, checked: false});
-        this.i = this.i + 1;
+      if (
+        !this.newActivityList.find(
+          activity => activity.value === this.newActivity.trim()
+        )
+      ) {
+        this.newActivityList.push({
+          id: this.newActivityListIndex,
+          value: this.newActivity.trim(),
+          checked: false
+        });
+        this.newActivityListIndex = this.newActivityListIndex + 1;
         this.newActivity = '';
       } else {
         this.done = true;
@@ -36,17 +47,36 @@ export class AppComponent {
       this.error = true;
       this.newActivity = '';
     }
-    // if (this.newActivityList.length) {
-    //   this.show = false;
-    // }
+  }
+
+  public showActivityList() {
+    this.error = false;
+    this.done = false;
+    this.selectedActivityArr = this.newActivityList.filter((activity) => {
+      return (activity.checked);
+    }).map(activity => activity.value );
+    if (!this.selectedActivityArr.length) {
+      this.notSelected = true;
+    } else {
+      this.notSelected = false;
+    }
+    console.log(this.selectedActivityArr);
+  }
+
+  public checkActivity(evt: any) {
+    console.log(this.newActivityList);
+    console.log(evt.source._elementRef.nativeElement.innerText);
+    console.log(evt.checked);
+    console.log(evt);
+    if (evt.checked) { this.strikeFlag = true; } else { this.strikeFlag = false; }
   }
 
   public clearAll() {
+    this.notSelected = false;
+    this.selectedActivityArr = [];
     this.newActivityList = [];
     this.newActivity = '';
     this.done = false;
     this.error = false;
-    // this.show = true;
   }
-
 }
